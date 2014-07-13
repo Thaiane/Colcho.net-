6d32e7abc69b0e3cc140286ee9d4fed55a8086ac
Thaiane <thaiane.unb@hotmail.com>
2014-07-13 18:54:14 -0300

    Implementação da utilização de senha encriptada

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :bio, :email, :full_name, :location, :password, :password_confirmation
 
-  validates_presence_of :email, :full_name, :location, :password
-  validates_confirmation_of :password
+  validates_presence_of :email, :full_name, :location
   validates_length_of :bio, :minimum => 30, :allow_blank => false
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   validates_uniqueness_of :email
+
+  has_secure_password
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140713022607) do
+ActiveRecord::Schema.define(:version => 20140713214052) do
 
   create_table "rooms", :force => true do |t|
     t.string   "title"
@@ -24,11 +24,11 @@ ActiveRecord::Schema.define(:version => 20140713022607) do
   create_table "users", :force => true do |t|
     t.string   "full_name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
