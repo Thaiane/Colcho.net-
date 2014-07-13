8fc6e3676be9b60f45a1696b373e1d22e8d62585
Thaiane <thaiane.unb@hotmail.com>
2014-07-13 12:42:13 -0300

    Aplicação atual até o capítulo 06 do livro.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colchonet::Application.config.secret_token = 'ff115144dc1448e6a6371a630029a2046b0db0bbe70bdd40d198ad6a21d16fb350bab62d262fe11dec4f5bf305906e89b25b9fa44a1cf2f23aa8566e62974b53'
